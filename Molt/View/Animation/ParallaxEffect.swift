7b2204e130eac7dee288e76d4157c21211574a70
prabal4546 <prabaljitwalia4546@gmail.com>
2021-11-06 00:35:27 +0530

    parallax effect

---
+++ Molt/View/Animation/ParallaxEffect.swift
@@ -0,0 +1,45 @@
+//
+//  ParallaxEffect.swift
+//  Molt
+//
+//  Created by Prabaljit Walia on 06/11/21.
+//
+
+import SwiftUI
+import CoreMotion
+
+struct ParallaxMotionModifier: ViewModifier {
+    
+    @ObservedObject var manager: MotionManager
+    var magnitude: Double
+    
+    func body(content: Content) -> some View {
+        content
+            .offset(x: CGFloat(manager.roll * magnitude), y: CGFloat(manager.pitch * magnitude))
+    }
+}
+
+class MotionManager: ObservableObject {
+
+    @Published var pitch: Double = 0.0
+    @Published var roll: Double = 0.0
+    
+    private var manager: CMMotionManager
+
+    init() {
+        self.manager = CMMotionManager()
+        self.manager.deviceMotionUpdateInterval = 1/60
+        self.manager.startDeviceMotionUpdates(to: .main) { (motionData, error) in
+            guard error == nil else {
+                print(error!)
+                return
+            }
+
+            if let motionData = motionData {
+                self.pitch = motionData.attitude.pitch
+                self.roll = motionData.attitude.roll
+            }
+        }
+
+    }
+}
