7cafa2b280a385ed245aaeb4e4ac1764cf8c8678
Ryan Lintott <ryan.lintott@abetterwaytodo.com>
2021-11-05 11:17:11 -0400

    Initial Commit

---
+++ Molt/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Molt
+//
+//  Created by Ryan Lintott on 2021-11-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Molt/MoltApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoltApp.swift
+//  Molt
+//
+//  Created by Ryan Lintott on 2021-11-05.
+//
+
+import SwiftUI
+
+@main
+struct MoltApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
