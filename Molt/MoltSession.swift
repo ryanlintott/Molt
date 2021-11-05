8f0d49d683cf83cf451b6bf02cb1b9f2e2a62a65
Ryan Lintott <ryan.lintott@abetterwaytodo.com>
2021-11-05 11:31:13 -0400

    Added MoltSession

---
+++ Molt/MoltSession.swift
@@ -0,0 +1,18 @@
+//
+//  MoltSession.swift
+//  Molt
+//
+//  Created by Ryan Lintott on 2021-11-05.
+//
+
+import Foundation
+
+struct MoltSession {
+    let dateStarted: Date
+    let length: TimeInterval
+    let goalLength: TimeInterval
+    
+    var completed: Bool {
+        length > goalLength
+    }
+}
