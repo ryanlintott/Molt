6a2c7032a74a3f7d721e836f7741f28c0fc1a1f6
Ryan Lintott <ryan.lintott@abetterwaytodo.com>
2021-11-06 18:19:01 -0400

    Created an InteractiveStickyView that can be dragged or tapped and will drop to the ground when released. curving moves with dragging motion. StickyWall updated with InteractiveStickyView. No more binding lists or swapping of elements. Size must be passed down to StickyWall view Added a basic sticky viewer to ContentView for now

--- Molt/View/StickyWall.swift
+++ Molt/View/StickyWall.swift
@@ -8,86 +8,32 @@
 import SwiftUI
 
 struct StickyWall: View {
-    @State var activeSticky: WallSticky? = nil
-    @Binding var stickies: [WallSticky]
+    let stickies: [WallSticky]
+    let frame: CGSize
     let stickySize: CGFloat = 100
-    let curveFactor: CGFloat
-    let phase: CGFloat
+    let dropHeight: CGFloat = 1000
     
     var body: some View {
-        GeometryReader { proxy in
-            ZStack {
-                Color.clear
-                
-                ForEach(stickies) { sticky in
-                    StickyView(color: sticky.color, curveFactor: abs((phase + sticky.curvePhase).truncatingRemainder(dividingBy: 2) - 1) * curveFactor)
-                        .frame(width: stickySize, height: stickySize)
-                        .position(
-                            x: proxy.size.width * sticky.x,
-                            y: proxy.size.height * sticky.y
-                        )
-                        .onTapGesture {
-                            activeSticky = sticky
-                            stickies.removeAll(where: { $0.id == sticky.id })
-                            withAnimation(.easeIn) {
-                                activeSticky?.y = proxy.size.height + stickySize
-                            }
-                        }
-                }
-                
-                if let sticky = activeSticky {
-                    StickyView(color: sticky.color, curveFactor: abs((phase + sticky.curvePhase).truncatingRemainder(dividingBy: 2) - 1) * curveFactor)
-                        .frame(width: stickySize, height: stickySize)
-                        .position(
-                            x: proxy.size.width * sticky.x,
-                            y: proxy.size.height * sticky.y
-                        )
-                }
+        ZStack {
+            Color.clear
+            
+            ForEach(stickies) { sticky in
+                InteractiveStickyView(color: sticky.color, size: stickySize, dropHeight: dropHeight, curvePhase: sticky.curvePhase)
+                    .position(
+                        x: frame.width * sticky.x,
+                        y: frame.height * sticky.y
+                    )
             }
         }
-        .border(.red)
-        .padding(.vertical, stickySize / 4)
+        .padding(.vertical, stickySize / 3)
+        .frame(width: frame.width, height: frame.height)
     }
 }
 
 struct StickyWall_Previews: PreviewProvider {
-    struct PreviewData: View {
-        @State private var stickies: [WallSticky] = .rainbow(count: 25)
-        
-        
-//        Color
-//            .stickyRainbow(count: 25)
-//            .enumerated()
-//            .map {
-//                WallSticky(
-//                    id: $0,
-//                    color: $1,
-//                    x: CGFloat.random(in: 0...1),
-//                    y: CGFloat.random(in: 0...1)
-//                )
-//            }
-//            .sorted(by: { lhs, rhs in
-//                lhs.y > rhs.y
-//            })
-        
-        @State private var phase: CGFloat = 0
-        
-        var body: some View {
-            StickyWall(stickies: $stickies, curveFactor: 0.2, phase: phase)
-                .overlay(
-                    Slider(value: $phase, in: 0...10)
-                        .padding()
-                    , alignment: .bottom
-                )
-                .onAppear {
-                    withAnimation(.linear(duration: 2)) {
-                        phase = 1
-                    }
-                }
-        }
-    }
-    
     static var previews: some View {
-        PreviewData()
+        GeometryReader { proxy in
+            StickyWall(stickies: .rainbow(count: 25), frame: proxy.size)
+        }
     }
 }
