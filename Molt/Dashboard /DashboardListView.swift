4e1486db9e4fcddab35445269ba5d4a3d96bd565
darshilagrawal <darshilagrawal2000@gmail.com>
2021-11-07 12:09:40 +0530

    Adding Dashboard File

---
+++ Molt/Dashboard /DashboardListView.swift
@@ -0,0 +1,23 @@
+//
+//  DashboardListView.swift
+//  Molt
+//
+//  Created by Darshil Agrawal on 06/11/21.
+//
+
+import SwiftUI
+
+struct DashboardListView: View {
+    var body: some View {
+        List{
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
+        
+    }
+}
+
+struct DashboardListView_Previews: PreviewProvider {
+    static var previews: some View {
+        DashboardListView()
+    }
+}
